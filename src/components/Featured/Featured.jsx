50451d66849b073827ea5bc4419ada16ee05b16e
sajib hossen <sajibhossensa90@gmail.com>
2022-04-14 03:38:34 +0600

    featued added

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
-import './Featured.scss';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import React from "react";
+import "./Featured.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import  KeyboardArrowUpIcon  from '@mui/icons-material/KeyboardArrowUp';
 
 const Featured = () => {
-    return (
-        <div className='featured'> 
-           <div className="top">
-               <div className="title">Total Revenue</div>
-               <MoreVertIcon fontSize='small' />
-           </div>
-           <div className="bottom"></div>
+  return (
+    <div className="featured">
+      <div className="top">
+        <div className="title">Total Revenue</div>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={75} text={"75%"} strokeWidth={5} />
         </div>
-    );
+        <p className="title">Total sales made Today</p>
+        <p className="amount">$450</p>
+        <p className="descri">
+          Previous transactions processing. Last payments may not be included
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negetive">
+                <KeyboardArrowDownIcon fontSize="small"  />
+              <div className="resultAmount">
+                $12.4
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+                <KeyboardArrowUpIcon fontSize="small" />
+              <div className="resultAmount">
+                $12.4
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpIcon fontSize="small" />
+              <div className="resultAmount">$12.4</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
